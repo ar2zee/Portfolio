12cee938efaa925261d0acb35a6076569a119eee
Artur Oganesyan <artur.og@yahoo.com>
2017-04-25 18:52:14 -0500

    Some Fixes with button ,  menu

--- js/common.js
+++ js/common.js
@@ -7,11 +7,25 @@ $('.main_head').css('height', $(window).height());	// fit all height
 	$(window).resize(function() {
 		heightDetect();
 	});
-	
+
 	$(".toggle_mnu").click(function() {
   		$(".sandwich").toggleClass("active");
 	});
 
+	$('.top_mnu ul a').click(function() {
+		$('.top_mnu').fadeOut(600);
+		$(".sandwich").toggleClass("active");
+	}).append('<span></span>');
+
+	$('.toggle_mnu').click(function() {
+		if ( $('.top_mnu').is(':visible') ) {
+			$('.top_mnu').fadeOut(600);
+			$('.top_mnu li a').removeClass('fadeInUp animated');
+		} else {
+			$('.top_mnu').fadeIn(600);
+			$('.top_mnu li a').addClass('fadeInUp animated');
+		}
+	});
 });
 
 $(window).load(function() { 
