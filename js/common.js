fb3c9a38b9a0a7295a0b04e0fbb4a9e6ac3bfa1b
Artur Oganesyan <artur.og@yahoo.com>
2017-04-25 12:49:12 -0500

    Added Pre-loader, resize-function, parallax

--- js/common.js
+++ js/common.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
-	
-	
+function heightDetect() {
+$('.main_head').css('height', $(window).height());	// fit all height
+};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+});
+
+$(window).load(function() { 
+	$(".loader_inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
 });
